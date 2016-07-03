5656a55d68889857d634f920cd59702fd17b0d86
Clément Aubin <clement.aubin@eisti.fr>
2016-07-04 00:15:33 +0200

    Added basic models without validators

--- network/models.py
+++ network/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Device(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    device_name = models.CharField(
+            max_length=25
+            validator=[validate_device_name])
+    device_ip = models.GenericIPAddressField(
+            protocol="IPv4",
+            unique=True)
+    description = models.CharField(
+            max_length=255,
+            blank=True)
+    add_date = models.DateTimeField(
+            "Date added",
+            auto_now_add=True)
+    last_modified = models.DateTimeField(
+            "Last update",
+            auto_now=True,
+            auto_now_add=True)
+
+    def __str__(self):
+        return self.device_name + " (" + self.description + ")"
+
+class Interface(models.Model):
+    WIFI = "WLP"
+    ETHERNET = "ETH"
+    INTERFACE_TYPE_CHOICES = (
+            (WIFI, "Wifi"),
+            (ETHERNET, "Ethernet")
+            )
+
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    interface_type = models.CharField(
+            max_length=3,
+            choices=INTERFACE_TYPE_CHOICES,
+            default=ETHERNET)
+    mac_address = models.CharField(
+            max_length=17
+            validator=[validate_mac_format]
+            unique=True)
+    description = models.CharField(
+            max_length=255,
+            blank=True)
+    add_date = models.DateTimeField(
+            "Date added",
+            auto_now_add=True)
+    last_modified = models.DateTimeField(
+            "Last update",
+            auto_now=True,
+            auto_now_add=True)
+
+    def __str__(self):
+        return (self.mac_address + " - " + self.interface_type
+        + " (" + self.description + ")")
