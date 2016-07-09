515018c581eddfb86b5aaee5ee416362e7f93193
Clément Aubin <clement.aubin@eisti.fr>
2016-07-10 00:11:51 +0200

    Added templates for config files rendering

--- network/settings.py
+++ network/settings.py
@@ -1,4 +1,14 @@
+""" Network IP prefix """
 IP_NETWORK_PREFIX = "192.168.253."
 IP_RANGE_START = 45
 IP_RANGE_END = 254
+
+""" NS options """
 DNS_DOMAIN = "salle106.atilla.org"
+DNS_DOMAIN_SEARCH = "members.salle106.atilla.org"
+DNS_SERVER_1 = "192.168.253.1"
+DNS_SERVER_2 = "192.168.253.1"
+
+""" General network options """
+DOMAIN_ROOT_SERVER = "192.168.253.1"
+DOMAIN_MAIL_SERVER = "192.168.253.1"
