dbeff1e3658ac0d976cb9d5202b790cf863cb1b0
Clément Aubin <clement.aubin@eisti.fr>
2016-07-13 16:57:10 +0200

    Fixed #2 ; improvements in templates

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from network.views import device
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
-    url(r'^network/', include('network.urls'))
+    url(r'^network/', include('network.urls')),
+    url(r'^$', device.DeviceView.as_view())
 ]
