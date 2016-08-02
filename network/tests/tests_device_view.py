e4bf02c2653209fe978a77efb8507a943e57cebd
Clément Aubin <clement.aubin@eisti.fr>
2016-08-02 19:22:34 +0200

    Refactored DeviceViewTestCase

--- network/tests/tests_device_view.py
+++ network/tests/tests_device_view.py
@@ -39,18 +39,54 @@ class DeviceViewTestCase(TestCase):
     def tearDown(self):
         self.test1.delete()
 
+    """ Generic test methods """
+
+    def deny_anonymous_test(self, view_name, args=[]):
+        response = self.anonymous.get(
+                reverse(view_name, args=args),
+                follow=True)
+        self.assertRedirects(
+                response,
+                (settings.LOGIN_URL + "?next=" + reverse(view_name,
+                                                         args=args)))
+
+
+    def load_test(self, view_name, template_name, args=[]):
+        response = self.client.get(
+                reverse(view_name, args=args),
+                follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, template_name)
+
+    def get_object_test(self, view_name):
+        """ The view should return a Device owned by the current user or
+        throw a 404 error """
+
+        """ Try with a device owned by the user """
+        response1 = self.client.get(reverse(
+            view_name,
+            args=[self.test1_device1.pk]))
+        self.assertTrue(self.test1_device1 == response1.context['device'])
+
+        """ Try with a device owned by another user """
+        response2 = self.client.get(reverse(
+            view_name,
+            args=[self.test2_device1.pk]))
+        self.assertTrue(response2.status_code == 404)
+
+        """ Try with a device that does not exists """
+        response3 = self.client.get(reverse(
+            view_name,
+            args=[42133742]))
+        self.assertTrue(response3.status_code == 404)
+
     """ Tests for DeviceView class """
 
     def test_view_deny_anonymous(self):
-        response = self.anonymous.get(reverse('network:index'), follow=True)
-        self.assertRedirects(
-                response,
-                settings.LOGIN_URL + "?next=" + reverse('network:index'))
+        self.deny_anonymous_test('network:index')
 
     def test_view_loads(self):
-        response = self.client.get(reverse('network:index'), follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'network/devices.html')
+        self.load_test('network:index', 'network/devices.html')
 
     def test_view_get_queryset(self):
         """ get_queryset should return an array of Devices """
@@ -62,62 +98,28 @@ class DeviceViewTestCase(TestCase):
     """ Tests for DeviceDetailView class """
 
     def test_detail_view_deny_anonymous(self):
-        response = self.anonymous.get(
-                reverse('network:device_detail',
-                        args=[self.test1_device1.pk]))
-        self.assertRedirects(
-                response,
-                (settings.LOGIN_URL + "?next="
-                    + reverse('network:device_detail',
-                              args=[self.test1_device1.pk])))
+        self.deny_anonymous_test(
+                'network:device_detail',
+                args=[self.test1_device1.pk])
 
     def test_detail_view_loads(self):
-        response = self.client.get(
-                reverse('network:device_detail',
-                        args=[self.test1_device1.pk]))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'network/device_detail.html')
+        self.load_test(
+                'network:device_detail',
+                'network/device_detail.html',
+                args=[self.test1_device1.pk])
 
     def test_detail_view_get_object(self):
-        """ The view should return a Device owned by the current user or
-        throw a 404 error """
-
-        """ Try with a device owned by the user """
-        response1 = self.client.get(reverse(
-            'network:device_detail',
-            args=[self.test1_device1.pk]))
-        self.assertTrue(self.test1_device1 == response1.context['device'])
-
-        """ Try with a device owned by another user """
-        response2 = self.client.get(reverse(
-            'network:device_detail',
-            args=[self.test2_device1.pk]))
-        self.assertTrue(response2.status_code == 404)
-
-        """ Try with a device that does not exists """
-        response3 = self.client.get(reverse(
-            'network:device_detail',
-            args=[42133742]))
-        self.assertTrue(response3.status_code == 404)
+        self.get_object_test('network:device_detail')
 
     """ Tests for DeviceCreateView class """
 
     def test_create_view_deny_anonymous(self):
-        response = self.anonymous.get(
-                reverse('network:device_create'),
-                follow=True)
-        self.assertRedirects(
-                response,
-                (settings.LOGIN_URL
-                    + "?next="
-                    + reverse('network:device_create')))
+        self.deny_anonymous_test('network:device_create')
 
     def test_create_view_loads(self):
-        response = self.client.get(
-                reverse('network:device_create'),
-                follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'network/device_form.html')
+        self.load_test(
+                'network:device_create',
+                'network/device_form.html')
 
     def test_create_view_form_valid(self):
         """ The registered device should have a unique name and should belong
@@ -153,44 +155,18 @@ class DeviceViewTestCase(TestCase):
     """ Tests for DeviceUpdateView class """
 
     def test_update_view_deny_anonymous(self):
-        response = self.anonymous.get(
-                reverse('network:device_update', args=[self.test1_device1.pk]),
-                follow=True)
-        self.assertRedirects(
-                response,
-                (settings.LOGIN_URL
-                    + "?next="
-                    + reverse('network:device_update',
-                              args=[self.test1_device1.pk])))
+        self.deny_anonymous_test(
+                'network:device_update',
+                args=[self.test1_device1.pk])
 
     def test_update_view_loads(self):
-        response = self.client.get(
-                reverse('network:device_update', args=[self.test1_device1.pk]),
-                follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'network/device_form.html')
+        self.load_test(
+                'network:device_update',
+                'network/device_form.html',
+                args=[self.test1_device1.pk])
 
     def test_update_view_get_object(self):
-        """ The view should return a Device owned by the current user or
-        throw a 404 error """
-
-        """ Try with a device owned by the user """
-        response1 = self.client.get(reverse(
-            'network:device_update',
-            args=[self.test1_device1.pk]))
-        self.assertTrue(self.test1_device1 == response1.context['device'])
-
-        """ Try with a device owned by another user """
-        response2 = self.client.get(reverse(
-            'network:device_update',
-            args=[self.test2_device1.pk]))
-        self.assertTrue(response2.status_code == 404)
-
-        """ Try with a device that does not exists """
-        response3 = self.client.get(reverse(
-            'network:device_update',
-            args=[42133742]))
-        self.assertTrue(response3.status_code == 404)
+        self.get_object_test('network:device_update')
 
     def test_update_view_commit(self):
         """ A device should only be editable by the device owner
@@ -207,44 +183,18 @@ class DeviceViewTestCase(TestCase):
     """ Test for DeviceDeleteView class """
 
     def test_delete_view_deny_anonymous(self):
-        response = self.anonymous.get(
-                reverse('network:device_delete', args=[self.test1_device1.pk]),
-                follow=True)
-        self.assertRedirects(
-                response,
-                (settings.LOGIN_URL
-                    + "?next="
-                    + reverse('network:device_delete',
-                              args=[self.test1_device1.pk])))
+        self.deny_anonymous_test(
+                'network:device_delete',
+                args=[self.test1_device1.pk])
 
     def test_delete_view_loads(self):
-        response = self.client.get(
-                reverse('network:device_delete', args=[self.test1_device1.pk]),
-                follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'network/device_confirm_delete.html')
+        self.load_test(
+            'network:device_delete',
+            'network/device_confirm_delete.html',
+            args=[self.test1_device1.pk])
 
     def test_delete_view_get_object(self):
-        """ The view should return a Device owned by the current user or
-        throw a 404 error """
-
-        """ Try with a device owned by the user """
-        response1 = self.client.get(reverse(
-            'network:device_delete',
-            args=[self.test1_device1.pk]))
-        self.assertTrue(self.test1_device1 == response1.context['device'])
-
-        """ Try with a device owned by another user """
-        response2 = self.client.get(reverse(
-            'network:device_delete',
-            args=[self.test2_device1.pk]))
-        self.assertTrue(response2.status_code == 404)
-
-        """ Try with a device that does not exists """
-        response3 = self.client.get(reverse(
-            'network:device_delete',
-            args=[42133742]))
-        self.assertTrue(response3.status_code == 404)
+        self.get_object_test('network:device_delete')
 
     def test_delete_view_commit(self):
         """ A device should only be deleted by the device owner
