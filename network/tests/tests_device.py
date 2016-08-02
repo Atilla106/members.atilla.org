f01c173a2697afcba03761ff069ee28ab05edac2
Clément Aubin <clement.aubin@eisti.fr>
2016-08-02 02:30:04 +0200

    Fixed typo

--- network/tests/tests_device.py
+++ network/tests/tests_device.py
@@ -85,5 +85,5 @@ class DeviceViewTestCase(TestCase):
         """ get_queryset should return an array of Devices """
         response = self.client.get(reverse('network:index'))
         device_list = response.context['user_devices_list']
-        self.assertTrue(all(isinstance(x, Device) for x in device_list)
+        self.assertTrue([all(isinstance(x, Device) for x in device_list)])
         self.assertTrue(device_list.count != 0)
