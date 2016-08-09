058d252f362df7774b42c6c98d3e0c7093484896
Clément Aubin <clement.aubin@eisti.fr>
2016-08-09 04:20:50 +0200

    Link account views with new LDAP package

--- accounts/ldap/utils.py
+++ accounts/ldap/utils.py
@@ -7,7 +7,7 @@ from django.conf import settings
 def generate_crypt_password(password):
     salt = crypt.mksalt(method=crypt.METHOD_SHA512)
     salt = '$1${}$'.format(salt)
-    return '{CRYPT}{}'.format(str(crypt.crypt(password, salt)))
+    return '{{CRYPT}}{}'.format(str(crypt.crypt(password, salt)))
 
 
 def get_biggest_LDAP_uid(connection):
@@ -28,8 +28,8 @@ def get_biggest_LDAP_uid(connection):
             result_type, result_data = connection.result(result_id, 0)
         except ldap.NO_SUCH_OBJECT:
             raise ldap.LDAPError(
-                "Distinguished name (%s) does not exist."
-                % settings.LDAP_USERS_BASE_DN)
+                'Distinguished name ({}) does not exist.'.format(
+                    settings.LDAP_USERS_BASE_DN))
 
         if result_type == ldap.RES_SEARCH_ENTRY:
             try:
