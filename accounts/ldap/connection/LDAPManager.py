94ff5eb020595a95a1310449cb1eaef7678078f4
Clément Aubin <clement.aubin@eisti.fr>
2016-08-14 21:23:02 +0200

    Add possibility to override LDAP connection object

--- accounts/ldap/connection/LDAPManager.py
+++ accounts/ldap/connection/LDAPManager.py
@@ -4,8 +4,9 @@ from .generic import LDAPGenericConnection
 
 
 class LDAPManagerConnection(LDAPGenericConnection):
-    def __init__(self):
+    def __init__(self, connection=None):
         return super(LDAPManagerConnection, self).__init__(
                 settings.LDAP_SERVER_URI,
                 settings.LDAP_MANAGEMENT_DN,
-                settings.LDAP_MANAGEMENT_PASSWORD)
+                settings.LDAP_MANAGEMENT_PASSWORD,
+                connection)
--- accounts/ldap/connection/generic.py
+++ accounts/ldap/connection/generic.py
@@ -4,9 +4,18 @@ from django.conf import settings
 
 
 class LDAPGenericConnection():
-    def __init__(self, server_uri, bind_dn=None, bind_password=None):
+    def __init__(
+            self,
+            server_uri,
+            bind_dn=None,
+            bind_password=None,
+            connection=None):
         self.check_LDAP_configuration()
-        self.connection = ldap.initialize(server_uri)
+
+        if connection is None:
+            self.connection = ldap.initialize(server_uri)
+        else:
+            self.connection = connection
 
         if bind_dn is not None and bind_password is not None:
             self.bind(bind_dn, bind_password)
