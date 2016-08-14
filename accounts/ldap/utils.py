79491f5361359bc8bd2daf6c8ecc73f589096ea7
Clément Aubin <clement.aubin@eisti.fr>
2016-08-14 02:19:55 +0200

    Add test_user_bind and change_user_password

--- accounts/ldap/utils.py
+++ accounts/ldap/utils.py
@@ -10,6 +10,31 @@ def generate_crypt_password(password):
     return '{{CRYPT}}{}'.format(str(crypt.crypt(password, salt)))
 
 
+def test_user_bind(user_dn, password):
+    try:
+        LDAPGenericConnection(
+                settings.LDAP_SERVER_URI,
+                user_dn,
+                password)
+        return True
+    except:
+        return False
+
+def change_user_password(user_dn, old_password, new_password):
+    try:
+        connection = LDAPGenericConnection(
+                settings.LDAP_SERVER_URI,
+                user_dn,
+                old_password)
+        mod_attrs = [(
+            ldap.MOD_ADD,
+            'userPassword',
+            generate_crypt_password(new_password))]
+        connection.modify_s(user_dn, mod_attrs)
+        return True
+    except:
+        return False
+
 def get_biggest_LDAP_uid(connection):
     search_filter = '(objectClass=posixAccount)'
     search_attribute = ["uidNumber"]
