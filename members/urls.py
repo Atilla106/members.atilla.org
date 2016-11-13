6ce5940b0e7067636830a8f8bdb46b5d06b45a01
Valentin Famelart <famelartva@eisti.eu>
2016-11-13 15:46:13 +0100

    Add legal page + link on register page

--- members/urls.py
+++ members/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from network.views import device
+from members.views import legal_view
 
 urlpatterns = [
+	url(r'^legal$', legal_view, name="legal"),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^network/', include('network.urls')),
