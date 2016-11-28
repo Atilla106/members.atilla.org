e7a51a214ef27ec0d4f259a2ea5bd888a4278705
Valentin Famelart <famelartva@eisti.eu>
2016-11-28 22:00:23 +0100

    Add has perm field to user list view

--- customAuthAdmin/auth/user/admin.py
+++ customAuthAdmin/auth/user/admin.py
@@ -12,7 +12,7 @@ def make_publisher(modeladmin, request, queryset):
         user.user_permissions.add(perm)
 
 
-make_publisher.short_description = "Add publish device permission"
+make_publisher.short_description = 'Ajouter les droits de publication'
 
 
 def remove_publisher(modeladmin, request, queryset):
@@ -23,12 +23,24 @@ def remove_publisher(modeladmin, request, queryset):
         user.user_permissions.remove(perm)
 
 
-remove_publisher.short_description = "Remove publish device permission"
+remove_publisher.short_description = 'Supprimer les droits de publication'
 
 
 class CustomUserAdmin(UserAdmin):
+    def is_allowed_publish_device(self, obj):
+        return obj.has_perm('network.can_publish_device')
+
+    is_allowed_publish_device.short_description = 'Peut publier'
+
     actions = [make_publisher, remove_publisher]
 
+    list_display = [
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_allowed_publish_device']
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
