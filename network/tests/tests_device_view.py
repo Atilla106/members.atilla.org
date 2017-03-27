164606a07483bd8fe8cba75c3f90db4b8537b1cc
Alexandre Terrien <terrienale@eisti.eu>
2017-03-27 19:27:01 +0200

    Fix tests

--- network/tests/tests_device_view.py
+++ network/tests/tests_device_view.py
@@ -153,7 +153,8 @@ class DeviceViewTestCase(TestCase):
 
         self.assertContains(
             response2,
-            'Invalid name',
+            "Le nom d'appareil ne peut pas contenir autre chose que des "
+            "caractères alphanumériques et '-' ",
             html=False,
         )
 
