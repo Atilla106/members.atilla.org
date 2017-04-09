3df12405cc23dd18ac17b51258d1df63125502d9
Valentin Famelart <famelartva@eisti.eu>
2017-04-09 15:28:37 +0200

    Move Account creation signal to signals.py

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import Permission, User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User, Permission
 
 from constance import config
+from .models import Account
 
 
 @receiver(post_save, sender=User)
@@ -11,3 +12,9 @@ def auto_add_publish_perm(sender, instance, created, **kwargs):
         perm = Permission.objects.get(codename='can_publish_device')
 
         instance.user_permissions.add(perm)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Account.objects.create(user=instance)
