38bb47bd93cb20a72923868d1d20d70fc6ffd593
Valentin Famelart <famelartva@eisti.eu>
2017-04-09 14:09:13 +0200

    Implement auto add perm logic

---
+++ accounts/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'members.accounts.app.AccountConfig'
---
+++ accounts/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User, Permission
+
+from constance import config
+
+
+@receiver(post_save, sender=User)
+def auto_add_publish_perm(sender, instance, created, **kwargs):
+    print("auto allow flag : {}".format(config.AUTO_ALLOW_FLAG))
+    if config.AUTO_ALLOW_FLAG and created:
+        perm = Permission.objects.get(codename='can_publish_device')
+
+        instance.user_permissions.add(perm)
